a701e2bd48a3d0a13579f1c17f521d390154db5c
Kieren Searle <kieren.searle@realwear.com>
2018-01-18 08:41:25 -0800

    Add index page
    Added the initial index page that explains what the project is

---
+++ index.ml
@@ -0,0 +1,21 @@
+---
+link: https://realwear.box.com/s/b4hmjso6yb7vn5pndkwfve1qmx7qj1gk
+---
+
+## Introduction
+
+Devil's Canyon is a simple application that is used to show how WearML can be used to optimize an Android App for a HMT-1 device.
+
+The application consists of a single screen containing 3 logos showing some delicious beers. Selecting each logo will show a short description about each beer.
+
+While the application is automatically voice enabled by the HMT-1, WearML can be used to provide many improvements to the user experience without any code changes.
+
+![Devil's Canyon application without any modifications](https://realwear.github.io/Devils-Canyon/images/devilscanyon.png)
+
+## Contents
+
+* DevilsCanyon.apk - The Devil's Canyon application without any modifications
+
+## [Tutorial](https://realwear.github.io/Devils-Canyon/tutorial)
+
+## [Release Notes](https://realwear.github.io/Devils-Canyon/release-notes)
